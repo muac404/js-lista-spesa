08ffbef700021413282680713d14b83696ca8633
muac404 <muac404@gmail.com>
2022-10-27 18:15:53 +0200

    Aggiunge lista della spesa html con ciclo while

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 "use strict"
+
+const shoppingList = [
+    "Pane",
+    "Vino",
+    "Acqua",
+    "Gin"
+];
+
+let i = 0
+
+while (i < shoppingList.length ){
+    
+    const container = document.querySelector(".container")
+    container.innerHTML = container.innerHTML +  `<li>${shoppingList[i]}</li>`
+
+    i++;
+}
